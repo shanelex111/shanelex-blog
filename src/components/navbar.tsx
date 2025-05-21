362629f66ecbee9531bf3d97a703bc1455f09c3f
xinyu.zhang <zhang.li@net263.com>
2025-05-21 18:16:25 +0800

    update navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { Search } from "lucide-react";
-
+import { Button } from "./ui/button";
+import { SiGithub } from "react-icons/si";
 export function Navbar() {
   return (
     <header className="w-full border-b bg-background/95 backdrop-blur sticky top-0 z-50">
@@ -10,15 +11,49 @@ export function Navbar() {
             ShaneLex
           </Link>
         </div>
-        <nav className="gap-6 md:flex hidden items-center font-medium text-lg">
-          <Link href="/">首页</Link>
-          <Link href="/articles">文章</Link>
-          <Link href="/projects">项目</Link>
-          <Link href="/about">关于</Link>
+        <nav className="gap-8 md:flex hidden items-center font-medium text-lg">
+          <Link href="/" className="transition-colors hover:text-primary">
+            首页
+          </Link>
+          <Link
+            href="/articles"
+            className="transition-colors hover:text-primary "
+          >
+            文章
+          </Link>
+          <Link
+            href="/projects"
+            className="transition-colors hover:text-primary "
+          >
+            项目
+          </Link>
+          <Link href="/about" className="transition-colors hover:text-primary ">
+            关于
+          </Link>
         </nav>
 
-        <div>
-          <Search className="h-5 w-5"></Search>
+        <div className="items-center gap-2 flex">
+          <Button
+            size="icon"
+            variant="ghost"
+            className="transition-all hover:text-primary hover:bg-transparent group"
+          >
+            <Search className="h-5 w-5 transition-transform group-hover:scale-110"></Search>
+          </Button>
+
+          <a
+            href="https://github.com/shanelex111"
+            target="_blank"
+            aria-label="GitHub"
+          >
+            <Button
+              size="icon"
+              variant="ghost"
+              className="transition-all hover:text-primary hover:bg-transparent group"
+            >
+              <SiGithub className="h-5 w-5 transition-transform group-hover:scale-110" />
+            </Button>
+          </a>
         </div>
       </div>
     </header>
